501be487d313f8b7af490ca38de38bce0acef135
CesarZacatales <00223021@uca.edu.sv>
2022-11-29 17:58:35 -0600

    segundo  commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,6 @@
 const Mongoose = require("mongoose");
 const debug = require("debug")("app:mongoose");
 
-const dbhost = process.env.DBHOST || "localhost";
-const dbport = process.env.DBPORT || "3500";
-const dbname = process.env.DBNAME || "wdyt-uca"
-
 const dburi = process.env.DBURI || `mongodb+srv://drodriguez22:wSNtlodv9Pe7pNwc@clusterelevenpage.2vfjs1z.mongodb.net/dbElevenPage?retryWrites=true&w=majority`;
 
 const connect = async () => {
